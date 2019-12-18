90fe8327c098e0720b7984d8b03102856d7a40f4
HOME's <HOME's@DESKTOP-HF90GH9>
2019-12-18 21:20:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using LibraryProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UserAccount> accounts = new List<UserAccount>();
+            var metode = new LibraryProject.Methodes();
+            
+            Console.WriteLine("Wellcome!");
+            if (accounts.Count == 0)
+            {
+                Console.WriteLine("There is no account!");
+                Console.WriteLine("Do you want to creat an account?(yes/no}");
+                string answear = Console.ReadLine();
+
+
+                do
+                {
+                    UserAccount userAccount = metode.CreateAccount();
+                    accounts.Add(userAccount);
+                    Console.WriteLine("Do you want to creat another account?(yes/no}");
+                    answear = Console.ReadLine();
+                } while (answear != "no");
+            }
+
+            Console.WriteLine("Do you want to login?");
+            string l = Console.ReadLine();
+            do
+            {
+                UserAccount ans = metode.LogIn(accounts);
+                if (ans != null)
+                {
+                    Console.WriteLine("Do you want to write a message?(yes/no)");
+                    string a = Console.ReadLine();
+                    if (a == "yes")
+                    {
+                        metode.GetUserMessage(ans);
+                    }
+                }
+            } while (l != "no");
+            
+
+
+        }
+    }
+}
